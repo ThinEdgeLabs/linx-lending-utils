f1bb7da9c24ca7bb1f12cb331425506c2bdc3a84
Radu Ciobanu <radu.ciobanu22@gmail.com>
2025-07-04 13:17:00 +0800

    Oracles cleanup

--- oracles/scripts/0_deploy_dia_oracle.ts
+++ oracles/scripts/0_deploy_dia_oracle.ts
@@ -1,19 +1,14 @@
 import { Deployer, DeployFunction, Network } from '@alephium/cli'
 import { Settings } from '../alephium.config'
 import { DIAOracleWrapper } from '../artifacts/ts'
-import { stringToHex } from '@alephium/web3'
 
-// This deploy function will be called by cli deployment tool automatically
-// Note that deployment scripts should prefixed with numbers (starting from 0)
-const deployFaucet: DeployFunction<Settings> = async (
+const deployOracle: DeployFunction<Settings> = async (
   deployer: Deployer,
   network: Network<Settings>
 ): Promise<void> => {
-  // Get settings
   const { diaOracleAddress, marketId, heartbeatInterval } = network.settings
 
   const result = await deployer.deployContract(DIAOracleWrapper, {
-    // The initial states of the faucet contract
     initialFields: {
       diaOracleAddress,
       marketId,
@@ -24,4 +19,4 @@ const deployFaucet: DeployFunction<Settings> = async (
   console.log('DIA Contract Wrapper contract address: ' + result.contractInstance.address)
 }
 
-export default deployFaucet
+export default deployOracle
