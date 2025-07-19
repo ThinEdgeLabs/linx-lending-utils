cb804ba72dcbb343f399b7cbda1550bb2d5089ed
Radu Ciobanu <radu.ciobanu22@gmail.com>
2025-07-19 15:38:12 +0800

    Cleanup

--- oracles/alephium.config.ts
+++ oracles/alephium.config.ts
@@ -1,33 +1,41 @@
 import { Configuration } from '@alephium/cli'
-import { HexString } from '@alephium/web3'
+import { addressFromContractId, HexString, NULL_CONTRACT_ADDRESS } from '@alephium/web3'
 
-// Settings are usually for configuring
 export type Settings = {
   diaOracleAddress: HexString
   marketId: HexString
   heartbeatInterval: bigint
 }
 
+const defaultSettings: Settings = {
+  diaOracleAddress: NULL_CONTRACT_ADDRESS,
+  marketId: '', // Market ID to be set at deployment, e.g. 'BTC/USD' in hex format
+  heartbeatInterval: 86400000n // 24 hours
+}
+
 const configuration: Configuration<Settings> = {
   networks: {
     devnet: {
-      nodeUrl: 'https://node.testnet.alephium.org',
+      nodeUrl: 'http://127.0.0.1:22973',
       privateKeys: [
         'a642942e67258589cd2b1822c631506632db5a12aabcf413604e785300d762a5' // group 0
       ],
-      settings: undefined as unknown as Settings
+      settings: defaultSettings
     },
 
     testnet: {
       nodeUrl: (process.env.NODE_URL as string) ?? 'https://node.testnet.alephium.org',
       privateKeys: process.env.PRIVATE_KEYS === undefined ? [] : process.env.PRIVATE_KEYS.split(','),
-      settings: undefined as unknown as Settings
+      settings: {
+        ...defaultSettings,
+        diaOracleAddress: addressFromContractId('216wgM3Xi5uBFYwwiw2T7iZoCy9vozPJ4XjToW74nQjbV') // DIA Oracle address on testnet
+      }
     },
 
     mainnet: {
       nodeUrl: (process.env.NODE_URL as string) ?? 'https://node.mainnet.alephium.org',
       privateKeys: process.env.PRIVATE_KEYS === undefined ? [] : process.env.PRIVATE_KEYS.split(','),
-      settings: undefined as unknown as Settings
+      settings: defaultSettings
     }
   }
 }
--- oracles/scripts/0_deploy_dia_oracle.ts
+++ oracles/scripts/0_deploy_dia_oracle.ts
@@ -1,22 +1,27 @@
 import { Deployer, DeployFunction, Network } from '@alephium/cli'
 import { Settings } from '../alephium.config'
 import { DIAOracleWrapper } from '../artifacts/ts'
+import { stringToHex } from '@alephium/web3'
 
 const deployOracle: DeployFunction<Settings> = async (
   deployer: Deployer,
   network: Network<Settings>
 ): Promise<void> => {
-  const { diaOracleAddress, marketId, heartbeatInterval } = network.settings
+  const { diaOracleAddress, heartbeatInterval, marketId } = network.settings
+
+  if (!marketId) {
+    throw new Error('Market ID is required to deploy the DIA Oracle Wrapper contract.')
+  }
 
   const result = await deployer.deployContract(DIAOracleWrapper, {
     initialFields: {
       diaOracleAddress,
-      marketId,
+      marketId: stringToHex(marketId),
       heartbeatInterval
     }
   })
-  console.log('DIA Contract Wrapper contract id: ' + result.contractInstance.contractId)
-  console.log('DIA Contract Wrapper contract address: ' + result.contractInstance.address)
+  console.log(`DIA ${marketId} Oracle contract id: ${result.contractInstance.contractId}`)
+  console.log(`DIA ${marketId} Contract Wrapper contract address: ${result.contractInstance.address}`)
 }
 
 export default deployOracle
