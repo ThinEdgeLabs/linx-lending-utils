27f761dca94664fa51e8593b2732a89e1201d07d
Radu Ciobanu <radu.ciobanu22@gmail.com>
2025-07-28 10:29:51 +0300

    Oracle returns price with correct scale

--- oracles/alephium.config.ts
+++ oracles/alephium.config.ts
@@ -1,15 +1,25 @@
 import { Configuration } from '@alephium/cli'
-import { addressFromContractId, HexString, NULL_CONTRACT_ADDRESS } from '@alephium/web3'
+import { HexString } from '@alephium/web3'
 
 export type Settings = {
-  diaOracleAddress: HexString
-  marketId: HexString
+  diaOracleContractId: HexString
   heartbeatInterval: bigint
+  baseMarketId: HexString // Market ID to be set at deployment, e.g. 'BTC/USD' in hex format
+  quoteMarketId: HexString // Market ID to be set at deployment, e.g. 'USDT/USD' in hex format
+  baseMarketDecimals: bigint // Number of decimals for the base asset price (e.g., 8 for BTC/USD DIA feed)
+  quoteMarketDecimals: bigint // Number of decimals for the quote asset price (e.g., 8 for USDT/USD DIA feed)
+  baseTokenDecimals: bigint // Number of decimals for the base token (e.g., 18 for WBTC)
+  quoteTokenDecimals: bigint // Number of decimals for the quote token (e.g., 6 for USDT)
 }
 
 const defaultSettings: Settings = {
-  diaOracleAddress: NULL_CONTRACT_ADDRESS,
-  marketId: '', // Market ID to be set at deployment, e.g. 'BTC/USD' in hex format
+  diaOracleContractId: '',
+  baseMarketId: '',
+  quoteMarketId: '',
+  baseMarketDecimals: 0n,
+  quoteMarketDecimals: 0n,
+  baseTokenDecimals: 0n,
+  quoteTokenDecimals: 0n,
   heartbeatInterval: 86400000n // 24 hours
 }
 
@@ -20,7 +30,16 @@ const configuration: Configuration<Settings> = {
       privateKeys: [
         'a642942e67258589cd2b1822c631506632db5a12aabcf413604e785300d762a5' // group 0
       ],
-      settings: defaultSettings
+      settings: {
+        ...defaultSettings,
+        diaOracleContractId: '216wgM3Xi5uBFYwwiw2T7iZoCy9vozPJ4XjToW74nQjbV',
+        baseMarketId: 'BTC/USD',
+        quoteMarketId: 'USDT/USD',
+        baseMarketDecimals: 8n,
+        quoteMarketDecimals: 6n,
+        baseTokenDecimals: 18n,
+        quoteTokenDecimals: 6n
+      }
     },
 
     testnet: {
@@ -28,7 +47,7 @@ const configuration: Configuration<Settings> = {
       privateKeys: process.env.PRIVATE_KEYS === undefined ? [] : process.env.PRIVATE_KEYS.split(','),
       settings: {
         ...defaultSettings,
-        diaOracleAddress: addressFromContractId('216wgM3Xi5uBFYwwiw2T7iZoCy9vozPJ4XjToW74nQjbV') // DIA Oracle address on testnet
+        diaOracleContractId: '216wgM3Xi5uBFYwwiw2T7iZoCy9vozPJ4XjToW74nQjbV' // DIA Oracle contract ID on testnet
       }
     },
 
--- oracles/scripts/0_deploy_dia_oracle.ts
+++ oracles/scripts/0_deploy_dia_oracle.ts
@@ -1,27 +1,60 @@
 import { Deployer, DeployFunction, Network } from '@alephium/cli'
 import { Settings } from '../alephium.config'
-import { DIAOracleWrapper } from '../artifacts/ts'
+import { LinxDiaOracle } from '../artifacts/ts'
 import { stringToHex } from '@alephium/web3'
+import { PrivateKeyWallet } from '@alephium/web3-wallet'
 
 const deployOracle: DeployFunction<Settings> = async (
   deployer: Deployer,
   network: Network<Settings>
 ): Promise<void> => {
-  const { diaOracleAddress, heartbeatInterval, marketId } = network.settings
+  const {
+    diaOracleContractId,
+    heartbeatInterval,
+    baseMarketId,
+    quoteMarketId,
+    baseMarketDecimals,
+    quoteMarketDecimals,
+    baseTokenDecimals,
+    quoteTokenDecimals
+  } = network.settings
 
-  if (!marketId) {
-    throw new Error('Market ID is required to deploy the DIA Oracle Wrapper contract.')
+  if (
+    !diaOracleContractId ||
+    !baseMarketId ||
+    !quoteMarketId ||
+    !baseMarketDecimals ||
+    !quoteMarketDecimals ||
+    !baseTokenDecimals ||
+    !quoteTokenDecimals ||
+    !heartbeatInterval
+  ) {
+    throw new Error('Missing required settings to deploy the LinxDIAOracle contract.')
   }
 
-  const result = await deployer.deployContract(DIAOracleWrapper, {
+  const result = await deployer.deployContract(LinxDiaOracle, {
     initialFields: {
-      diaOracleAddress,
-      marketId: stringToHex(marketId),
-      heartbeatInterval
+      diaOracleContractId,
+      baseMarketId: stringToHex(baseMarketId),
+      quoteMarketId: stringToHex(quoteMarketId),
+      heartbeatInterval,
+      scaleFactor: 0n, // This will be calculated in the contract's init function
+      baseMarketDecimals,
+      quoteMarketDecimals,
+      baseTokenDecimals,
+      quoteTokenDecimals
     }
   })
-  console.log(`DIA ${marketId} Oracle contract id: ${result.contractInstance.contractId}`)
-  console.log(`DIA ${marketId} Contract Wrapper contract address: ${result.contractInstance.address}`)
+  console.log(`${baseMarketId} oracle contract id: ${result.contractInstance.contractId}`)
+  console.log(`${baseMarketId} oracle contract address: ${result.contractInstance.address}`)
+
+  const privateKey = network.privateKeys[0]
+  const signer = new PrivateKeyWallet({ privateKey })
+  LinxDiaOracle.at(result.contractInstance.address).transact.init({
+    signer
+  })
+
+  console.log(`Initialized ${baseMarketId} oracle contract at address: ${result.contractInstance.address}`)
 }
 
 export default deployOracle
