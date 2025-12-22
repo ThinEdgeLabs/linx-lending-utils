efa6cc57335b0feb4ea3cb38693f06fd8bb88ea4
Radu Ciobanu <radu.ciobanu22@gmail.com>
2025-12-22 23:30:38 +0800

    Deployment scripts

--- oracles/scripts/0_deploy_alph_usdt_dia_oracle.ts
+++ oracles/scripts/0_deploy_alph_usdt_dia_oracle.ts
@@ -1,7 +1,7 @@
 import { Deployer, DeployFunction, Network } from '@alephium/cli'
 import { Settings } from '../alephium.config'
 import { ALPHUSDTOracle } from '../artifacts/ts'
-import { stringToHex } from '@alephium/web3'
+import { stringToHex, waitForTxConfirmation } from '@alephium/web3'
 import { PrivateKeyWallet } from '@alephium/web3-wallet'
 
 const deployOracle: DeployFunction<Settings> = async (
@@ -39,15 +39,22 @@ const deployOracle: DeployFunction<Settings> = async (
   console.log(`${oracleSettings.baseMarketId} oracle contract id: ${result.contractInstance.contractId}`)
   console.log(`${oracleSettings.baseMarketId} oracle contract address: ${result.contractInstance.address}`)
 
+  await waitForTxConfirmation(result.txId, 1, 3)
+
   const privateKey = network.privateKeys[0]
   const signer = new PrivateKeyWallet({ privateKey })
-  ALPHUSDTOracle.at(result.contractInstance.address).transact.init({
+
+  const initResult = await ALPHUSDTOracle.at(result.contractInstance.address).transact.init({
     signer
   })
+  await waitForTxConfirmation(initResult.txId, 1, 3)
 
   console.log(
     `Initialized ${oracleSettings.baseMarketId} oracle contract at address: ${result.contractInstance.address}`
   )
+
+  const priceFetchResult = await ALPHUSDTOracle.at(result.contractInstance.address).view.price()
+  console.log(`Fetched initial price oracle: ${priceFetchResult.returns.toString()}`)
 }
 
 export default deployOracle
--- oracles/scripts/1_deploy_wbtc_usdt_dia_oracle.ts
+++ oracles/scripts/1_deploy_wbtc_usdt_dia_oracle.ts
@@ -1,7 +1,7 @@
 import { Deployer, DeployFunction, Network } from '@alephium/cli'
 import { Settings } from '../alephium.config'
 import { WBTCUSDTOracle } from '../artifacts/ts'
-import { stringToHex } from '@alephium/web3'
+import { stringToHex, waitForTxConfirmation } from '@alephium/web3'
 import { PrivateKeyWallet } from '@alephium/web3-wallet'
 
 const deployOracle: DeployFunction<Settings> = async (
@@ -39,16 +39,23 @@ const deployOracle: DeployFunction<Settings> = async (
   console.log(`${oracleSettings.baseMarketId} oracle contract id: ${result.contractInstance.contractId}`)
   console.log(`${oracleSettings.baseMarketId} oracle contract address: ${result.contractInstance.address}`)
 
+  await waitForTxConfirmation(result.txId, 1, 3)
+
   const privateKey = network.privateKeys[0]
   const signer = new PrivateKeyWallet({ privateKey })
 
-  WBTCUSDTOracle.at(result.contractInstance.address).transact.init({
+  const initResult = await WBTCUSDTOracle.at(result.contractInstance.address).transact.init({
     signer
   })
+
+  await waitForTxConfirmation(initResult.txId, 1, 3)
 
   console.log(
     `Initialized ${oracleSettings.baseMarketId} oracle contract at address: ${result.contractInstance.address}`
   )
+
+  const priceFetchResult = await WBTCUSDTOracle.at(result.contractInstance.address).view.price()
+  console.log(`Fetched initial price oracle: ${priceFetchResult.returns.toString()}`)
 }
 
 export default deployOracle
